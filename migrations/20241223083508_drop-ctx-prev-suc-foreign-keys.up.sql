bae5bf529d1bae08bfc6db056b1d0d22e6b55cea
Max Kaye <m@xk.io>
2025-01-17 02:04:54 +1100

    fix migration

--- migrations/20241223083508_drop-ctx-prev-suc-foreign-keys.up.sql
+++ migrations/20241223083508_drop-ctx-prev-suc-foreign-keys.up.sql
@@ -1,7 +1,7 @@
 -- Add up migration script here
 -- constraints to drop contexts_successor_fkey, contexts_predecessor_fkey
-ALTER TABLE contexts DROP CONSTRAINT IF EXISTS contexts_successor_fkey;
-ALTER TABLE contexts DROP CONSTRAINT IF EXISTS contexts_predecessor_fkey;
-ALTER TABLE vehicle_states DROP CONSTRAINT IF EXISTS vehicle_states_context_id_fkey;
-ALTER TABLE game_cam_nods DROP CONSTRAINT IF EXISTS game_cam_nods_context_id_fkey;
-ALTER TABLE position_reports DROP CONSTRAINT IF EXISTS position_reports_context_id_fkey;
+ALTER TABLE IF EXISTS contexts DROP CONSTRAINT IF EXISTS contexts_successor_fkey;
+ALTER TABLE IF EXISTS contexts DROP CONSTRAINT IF EXISTS contexts_predecessor_fkey;
+ALTER TABLE IF EXISTS vehicle_states DROP CONSTRAINT IF EXISTS vehicle_states_context_id_fkey;
+ALTER TABLE IF EXISTS game_cam_nods DROP CONSTRAINT IF EXISTS game_cam_nods_context_id_fkey;
+ALTER TABLE IF EXISTS position_reports DROP CONSTRAINT IF EXISTS position_reports_context_id_fkey;
