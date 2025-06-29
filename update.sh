add2474c5eae4443030c0c312ddbc56f434bc62d
Max Kaye <m@xk.io>
2025-06-29 18:09:31 +1000

    update sleep in update.sh so it shows more logs around thhe restart

--- update.sh
+++ update.sh
@@ -7,10 +7,8 @@ systemctl status dd2-server | cat
 systemctl restart dd2-server
 systemctl status dd2-server | cat
 
-sleep 1
-
 echo "opening logs in follow mode.."
 
-sleep 1
+sleep 0.25
 
 journalctl -fu dd2-server
